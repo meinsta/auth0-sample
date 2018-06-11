352844463d865d8680d08de41d4d1afa1b11125d
Meina <meinsta@gmail.com>
2018-06-11 15:57:56 -0400

    create a basic employee table with dummy data, remove canned react code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import React, { Component, PropTypes } from 'react';
-import { render } from 'react-dom';
-import SortableTable from 'react-sortable-table';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import _ from 'lodash';
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {      
+      data: data
+    };
+  }
+  render() {    
+    let sorted = this.state.data;
+     
+    const items = sorted.map((item)=>{
+      return <Employee data={ item } key={ item.id } />
+    }); 
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="row">
+          <div className="row-rapper">
+            { items } 
+          </div>          
+        </div>                
       </div>
-    );
+    )
   }
 }
+
+class Employee extends React.Component {
+  render(){
+    const { data, orderBy } = this.props;
+    const input = categories; 
+    const output = input.map((item)=>{
+      return <div><small className={ orderBy === item ? "active" : null }>{item}:</small> {data[item] }</div>
+    });
+    return (    
+      <div className="card">
+        <div className="employee">
+          <img className="avatar" src={ data.img } alt={`${data.first} ${data.last}` } /> 
+        </div>
+        <div className="details">
+          <h4>
+            <span className={ orderBy === "first" ? "active" : null }>{ data.first } </span> 
+            <span className={ orderBy === "last" ? "active" : null }>{ data.last }</span>
+          </h4>      
+          {output}
+        </div>
+      </div>     
+    )
+  }
+}
+const categories = ["position"]
+const data = [{"id":1,"first":"Yasmine","last":"Ahmed","img":"https://randomuser.me/api/portraits/women/99.jpg","position":"Analyst"},
+{"id":2,"first":"Gerardo","last":"Burmudez","img":"https://randomuser.me/api/portraits/men/2.jpg","position":"Engineer"},
+{"id":3,"first":"Juan","last":"Armstrong","img":"https://randomuser.me/api/portraits/men/49.jpg","position":"Designer"},
+{"id":4,"first":"Nooshin","last":"Mizrahi","img":"https://randomuser.me/api/portraits/women/36.jpg","position":"Producer"},
+{"id":5,"first":"Tamar","last":"Cohen","img":"https://randomuser.me/api/portraits/women/59.jpg","position":"Accountant"}];
 
 export default App;
